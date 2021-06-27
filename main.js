a59173e3911048d5cd19d85859d4876fcc4c1c4d
Alan Yao <alanyao.training@gmail.com>
2021-06-27 15:23:04 -0700

    Text

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -11,9 +11,12 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setX(-3);
 camera.position.setZ(30);
 
 renderer.render( scene, camera );
+
+// Torus
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
 const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
@@ -21,17 +24,21 @@ const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus);
 
+// Lighting
+
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set( 5, 5, 5 );
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add( pointLight, ambientLight );
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper);
 
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+// Stars
 
 function addStar() {
   const geometry = new THREE.SphereGeometry( 0.25, 24, 24 );
@@ -46,8 +53,45 @@ function addStar() {
 
 Array(200).fill().forEach(addStar);
 
+// Background
+
 const spaceTexture = new THREE.TextureLoader().load('space.jpeg');
 scene.background = spaceTexture;
+
+//Avatar
+
+const alanTexture = new THREE.TextureLoader().load('alan.jpg');
+
+const alan = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial( { map: alanTexture} )
+);
+
+scene.add(alan);
+
+alan.position.x = (2);
+alan.position.z = (-5);
+
+// Sun
+
+const sunTexture = new THREE.TextureLoader().load('sun.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const sun = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial( {
+    map: sunTexture,
+    normalMap: normalTexture
+  } )
+)
+
+scene.add(sun);
+
+sun.position.setX(-10);
+sun.position.z = (30);
+
+
+// Functions
 
 function animate() {
   requestAnimationFrame( animate );
@@ -56,9 +100,31 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
-  controls.update();
+  sun.rotation.x += 0.005;
+
+  // alan.rotation.x -= 0.001;
+  alan.rotation.y += 0.0015;
+  // controls.update();
 
   renderer.render( scene, camera );
 }
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  sun.rotation.x += 0.05;
+  sun.rotation.y += 0.075;
+  sun.rotation.z += 0.05;
+
+  alan.rotation.y += 0.01;
+  alan.rotation.z += 0.01;
+
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+  camera.position.z = t * -0.01;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
 
 animate();
