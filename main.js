3d7bce5291965991c8ce0fa853a26ba8af4c990d
Alan Yao <alanyao.training@gmail.com>
2021-06-27 13:44:48 -0700

    Fix

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
