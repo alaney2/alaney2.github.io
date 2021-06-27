004e936779002585cca0b722583915b9830a48c4
Alan Yao <alanyao.training@gmail.com>
2021-06-27 16:24:48 -0700

    Remove node modules

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import './style.css';
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+// import * as THREE from 'three';
+
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
