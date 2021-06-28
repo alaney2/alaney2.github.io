20082dd02cf26ee972d6b928d4e8475bbf811910
Alan Yao <alanyao.training@gmail.com>
2021-06-27 17:23:40 -0700

    Clean up

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-// import * as THREE from 'three';
-
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -32,12 +29,6 @@ pointLight.position.set( 5, 5, 5 );
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add( pointLight, ambientLight );
-
-// const lightHelper = new THREE.PointLightHelper(pointLight);
-// const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(lightHelper, gridHelper);
-
-// const controls = new OrbitControls(camera, renderer.domElement);
 
 // Stars
 
@@ -103,9 +94,7 @@ function animate() {
 
   sun.rotation.x += 0.005;
 
-  // alan.rotation.x -= 0.001;
   alan.rotation.y += 0.0015;
-  // controls.update();
 
   renderer.render( scene, camera );
 }
